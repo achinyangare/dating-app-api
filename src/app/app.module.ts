d877d4dad66500b2897691d55dba2ce9fe0f7e12
Allen Chinyangare <chinyangarea@payserv.co.zw>
2018-12-23 09:25:15 +0200

    Updated home, register and nav components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {ValueComponent} from './value/value.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NavComponent} from './nav/nav.component';
+import {FormsModule} from '@angular/forms';
+import {AuthenticationService} from './_services/authentication.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ValueComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ValueComponent,
+        NavComponent,
+        HomeComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule
+    ],
+    providers: [
+        AuthenticationService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
